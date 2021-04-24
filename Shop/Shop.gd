d559863c0e669d07383cddaa0e0672bdb10d5264
Decremented Palindrome <decpal@decrementedpalindrome.com>
2021-04-24 23:03:46 +0100

    Improve coin texture and allow the player to buy an upgrade if they have exactly the right amount of money

--- Shop/Shop.gd
+++ Shop/Shop.gd
@@ -5,13 +5,13 @@ func _on_Continue_pressed():
 
 
 func _on_FireRate_pressed():
-	if Global.money > Upgrades.fire_rate[Upgrades.fire_rate_level+1].cost:
+	if Global.money >= Upgrades.fire_rate[Upgrades.fire_rate_level+1].cost:
 		Global.money -= Upgrades.fire_rate[Upgrades.fire_rate_level+1].cost
 		Upgrades.fire_rate_level += 1
 		$FireRate.calculate_visibility()
 		
 func _on_Health_pressed():
-	if Global.money > Upgrades.health[Upgrades.health_level+1].cost:
+	if Global.money >= Upgrades.health[Upgrades.health_level+1].cost:
 		Global.money -= Upgrades.health[Upgrades.health_level+1].cost
 		Upgrades.health_level += 1
 		$Health.calculate_visibility()
